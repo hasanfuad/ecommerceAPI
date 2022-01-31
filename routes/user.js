965ee17d1846cc6c193f3ad1b4d3727af4e65378
hasanfuad <hasanmdfuad@hotmail.com>
2022-01-31 14:09:02 +0400

    commit delete, get and get all router

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,9 @@
 const User = require("../models/User");
-const { verifyToken } = require("./verifyToken");
+const {
+  verifyToken,
+  verifyTokenAndAuthorization,
+  verifyTokenAndAdmin,
+} = require("./verifyToken");
 
 const router = require("express").Router();
 
@@ -21,6 +25,44 @@ router.put("/:id", verifyToken, async (req, res) => {
       { new: true }
     );
     res.status(200).json(updatedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// DELETE
+
+router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).json("User has been deleted!");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// GET USER
+
+router.get("/find/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, ...others } = user._doc;
+
+    res.status(200).json(others);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// GET ALL USERS
+
+router.get("/", verifyTokenAndAdmin, async (req, res) => {
+  const query = req.query.new;
+  try {
+    const users = query
+      ? await User.find().sort({ _id: -1 }).limit(5)
+      : await User.find();
+    res.status(200).json(users);
   } catch (err) {
     res.status(500).json(err);
   }
